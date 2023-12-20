bfa5f4a9a0ddb7aa08567a8d9f6832c644af1891
José Montenegro <3013297960101@ingenieria.usac.edu.gt>
2023-12-20 00:24:57 -0600

    Agregaron archivo de python para generar archivo tflite

---
+++ tflite_model.py
@@ -0,0 +1,20 @@
+from tflite_model_maker import image_classifier
+from tflite_model_maker.image_classifier import DataLoader
+
+import tensorflow as tf
+assert tf.__version__.startswith('2')
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seedir as sd
+
+image_path = './dinosaur_photo'
+sd.seedir(image_path, style='spaces', indent=4, anystart='- ')
+data = DataLoader.from_folder(image_path)
+train_data, test_data = data.split(0.9)
+
+model = image_classifier.create(train_data)
+
+loss, accuracy = model.evaluate(test_data)
+
+model.export(export_dir='.')
